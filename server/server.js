51b6db5a8f42e1b0e41750ba2f42c7afd64f8147
Surya-0804 <surya.abothula@gmail.com>
2025-05-07 01:49:00 +0530

    Feat: Implement user authentication with registration and login functionality

---
+++ server/server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+import authRoutes from './routes/authRoutes.js';
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+connectDB();
+
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+app.use('/auth', authRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port http://localhost:${PORT}`);
+});
---
+++ server/utils/generateToken.js
@@ -0,0 +1,7 @@
+import jwt from 'jsonwebtoken';
+
+export const generateToken = (id, email, username) => {
+  return jwt.sign({ id, email, username }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  });
+};
